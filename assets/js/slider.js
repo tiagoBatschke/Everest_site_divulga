7d52aa5c63b3af355390649617651c49a68bfb21
tiagoBatschke <83837600+tiagoBatschke@users.noreply.github.com>
2023-10-03 13:06:26 -0300

    att

---
+++ assets/js/slider.js
@@ -0,0 +1,56 @@
+/*=============== Slick===============*/
+
+$(document).ready(function() {
+    $(".regular").slick({
+      dots: true,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      appendArrows: $(".custom-arrows"),
+      prevArrow: '<div class="container_prev"><button class="slick-prev">‹</button></div>',
+      nextArrow: '<div class="container_next"><button class="slick-next">›</button></div>',
+      responsive: [
+        {
+          breakpoint: 2000,
+          settings: {
+            slidesToShow: 4.5,
+            slidesToScroll: 4.5,
+            infinite: false,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 980,
+          settings: {
+            slidesToShow: 3.5,
+            slidesToScroll: 3.5,
+            infinite: false,
+          }
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 2.5,
+            slidesToScroll: 2.5,
+            infinite: false,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: false,
+          }
+        },
+        {
+          breakpoint: 450,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+          }
+        },
+      ]
+    });
+  });
